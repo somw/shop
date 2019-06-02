ee5e87d1158d720d679a3a602c979f1ae6588a09
somw <somw@qq.com>
2019-06-02 09:49:59 +0800

    a

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,11 +8,11 @@ class Index extends Base
     	// 热卖商品
     	// dump($this->config);die;
         //$this->hotGoods(); 
-        $recShopcate = model('shopcate')->getRecShopcate(5,0);  //首页推荐 推荐位顶级分类
+        $recShopcate = model('shopcate')->getRecShopcate(4,0);  //首页推荐 推荐位顶级分类
         // dump($recShopcate);die;
         foreach ($recShopcate as $k => $v) {
         	// 获取当前顶级分类下被设为 首页推荐 的二级分类
-        	$recShopcate[$k]['children'] = model('shopcate')->getRecShopcate(5,$v['shopcate_id']);
+        	$recShopcate[$k]['children'] = model('shopcate')->getRecShopcate(4,$v['shopcate_id']);
         	// 获取新品推荐
         	// 1、
         	
