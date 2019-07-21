8dadc5e160621b8e9f6854cca0f9efd509b82c45
somw <somw@qq.com>
2019-07-21 16:49:47 +0800

    16:50

---
+++ application/member/controller/Accout.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\member\controller;
+use think\Controller;
+class Accout extends Controller
+{
+
+	public function reg()
+	{
+		return view();
+	}
+
+	public function login()
+	{
+		return view();
+	}
+}
