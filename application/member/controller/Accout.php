5bcde665b747803ff99f61917f9b75b1a088ec36
somw <somw@qq.com>
2019-07-28 16:57:03 +0800

    123

--- application/member/controller/Accout.php
+++ application/member/controller/Accout.php
@@ -1,16 +1,29 @@
 <?php
 namespace app\member\controller;
 use think\Controller;
+use sms\Sms;
 class Accout extends Controller
 {
 
 	public function reg()
 	{
+		if (request()->isPost()) {
+			$data = input('post.');
+
+			$regadd = db('user')->insert();
+			if ($regadd) {
+				$this->success('注册成功！');
+			}else{
+				$this->error('注册失败');
+			}
+		}
 		return view();
 	}
 
 	public function login()
 	{
+
+
 		return view();
 	}
 }
